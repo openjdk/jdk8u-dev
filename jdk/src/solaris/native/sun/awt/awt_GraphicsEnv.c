8ce0db6addf0c3ccf14216e17e0b6071fe45c128
Zhengyu Gu <zgu@openjdk.org>
2022-04-05 00:39:02 +0000

    8284033: Leak XVisualInfo in getAllConfigs in awt_GraphicsEnv.c
    Reviewed-by: serb, prr

--- jdk/src/solaris/native/sun/awt/awt_GraphicsEnv.c
+++ jdk/src/solaris/native/sun/awt/awt_GraphicsEnv.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -576,6 +576,8 @@ getAllConfigs (JNIEnv *env, int screen, AwtScreenDataPtr screenDataPtr) {
        XFree (pVI8sg);
     if (n1sg != 0)
        XFree (pVI1sg);
+    if (nTrue != 0)
+       XFree (pVITrue);
 
     screenDataPtr->numConfigs = nConfig;
     screenDataPtr->configs = graphicsConfigs;
