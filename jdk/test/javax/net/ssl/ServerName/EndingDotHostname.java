491b6b93407673cddfafba96c7d3c96cb731e69e
Roman Marchenko <rmarchenko@azul.com>
2023-01-18 10:42:25 +0200

    Fixed strings format and hosts file substitution

--- jdk/test/javax/net/ssl/ServerName/EndingDotHostname.java
+++ jdk/test/javax/net/ssl/ServerName/EndingDotHostname.java
@@ -26,9 +26,8 @@
  * @bug 8065422
  * @summary Trailing dot in hostname causes TLS handshake to fail
  * @library /javax/net/ssl/templates
- * @run main/othervm --add-opens java.base/sun.security.ssl=ALL-UNNAMED
- *      -Djdk.net.hosts.file=hostsForExample EndingDotHostname
- */
+ * @run main/othervm EndingDotHostname
+*/
 
 import javax.net.ssl.*;
 import java.io.InputStream;
