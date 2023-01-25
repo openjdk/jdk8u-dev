d5ec0a9a762f282d4666236717f7067129e2dfa1
Jiri Vanek <jvanek@redhat.com>
2023-01-25 15:29:27 +0100

    Revert "repalce [] by test, mkaing older ubuntu happy"
    This reverts commit e1fda5545258fc1b646392d7417b2e2e29292de3.

--- jdk/test/jdk/jfr/event/sampling/TestNative.sh
+++ jdk/test/jdk/jfr/event/sampling/TestNative.sh
@@ -3,26 +3,26 @@
 set -ex
 
 OS=`uname -s`
-if test  "${OS}" != "Linux" -a "${OS}" != "Darwin" ; then
+if [  "${OS}" != "Linux" -a "${OS}" != "Darwin" ]; then
     echo "This is a Linux/MacOSX only test"
     exit 0;
 fi
 
-if test "x$TESTGCC" == "x" ; then
+if [ "x$TESTGCC" == "x" ]; then
   TESTGCC=$(readlink -f $(which gcc))
 fi
 
-if test "x$TESTGCC" == "x" ; then
+if [ "x$TESTGCC" == "x" ]; then
   echo "WARNING: gcc not found. Cannot execute test." 2>&1
   exit 1;
 fi
 
-if test "x$TESTROOT" == "x" ; then
+if [ "x$TESTROOT" == "x" ]; then
   echo "TESTROOT pointintg to top level sources is not set. that is fatal"
   exit 2;
 fi
 
-if test "x$TESTJAVA" == "x" ; then
+if [ "x$TESTJAVA" == "x" ]; then
   TESTJAVA=$(dirname $(dirname $(readlink -f $(which java))))
 fi
 
