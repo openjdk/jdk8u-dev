f826982e8ce1ac3b46d1de14894080b315339766
Alexey Bakhtin <alexey@azul.com>
2023-10-11 12:06:59 -0700

    Backport 47951655acacba515c0d69f5192257664f887dba

--- jdk/src/share/classes/sun/security/util/DisabledAlgorithmConstraints.java
+++ jdk/src/share/classes/sun/security/util/DisabledAlgorithmConstraints.java
@@ -158,6 +158,9 @@ public class DisabledAlgorithmConstraints extends AbstractAlgorithmConstraints {
     @Override
     public final boolean permits(Set<CryptoPrimitive> primitives,
             String algorithm, AlgorithmParameters parameters) {
+        if (algorithm == null || algorithm.isEmpty()) {
+            throw new IllegalArgumentException("No algorithm name specified");
+        }
         if (!checkAlgorithm(disabledAlgorithms, algorithm, decomposer)) {
             return false;
         }
