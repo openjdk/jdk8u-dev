6f1ae0bca857cb8d78d25b6fef78846ad1e39947
Takakuri <k.takakuri@fujitsu.com>
2024-05-07 21:13:06 +0900

    Backport 9158c1d9b686179742f4b129a9aee02c95458a58

--- jdk/test/com/sun/jdi/ModificationWatchpoints.java
+++ jdk/test/com/sun/jdi/ModificationWatchpoints.java
@@ -31,7 +31,7 @@
  *
  *  @run build JDIScaffold VMConnection
  *  @run compile -g ModificationWatchpoints.java
- *  @run main/othervm ModificationWatchpoints
+ *  @run driver ModificationWatchpoints
  */
 import com.sun.jdi.*;
 import com.sun.jdi.event.*;
