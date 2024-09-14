2b324f25d11f40fb1f0554149826b91369b342e1
daiyi <daiyi@ly-sky.com>
2024-09-14 17:30:29 +0800

    fix: unnecessary logical conditions

--- jdk/src/share/classes/java/util/concurrent/ThreadPoolExecutor.java
+++ jdk/src/share/classes/java/util/concurrent/ThreadPoolExecutor.java
@@ -1137,9 +1137,7 @@ public class ThreadPoolExecutor extends AbstractExecutorService {
                 // if not, ensure thread is not interrupted.  This
                 // requires a recheck in second case to deal with
                 // shutdownNow race while clearing interrupt
-                if ((runStateAtLeast(ctl.get(), STOP) ||
-                     (Thread.interrupted() &&
-                      runStateAtLeast(ctl.get(), STOP))) &&
+                if (runStateAtLeast(ctl.get(), STOP) &&
                     !wt.isInterrupted())
                     wt.interrupt();
                 try {
