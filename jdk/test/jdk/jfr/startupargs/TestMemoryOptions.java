a2460eba2a3c2f3640af71c7bb2c292f298fa545
KIRIYAMA Takuya <kiriyama.takuya@fujitsu.com>
2025-03-26 17:05:13 +0900

    8352942: jdk/jfr/startupargs/TestMemoryOptions.java fails with 32-bit build

--- jdk/test/jdk/jfr/startupargs/TestMemoryOptions.java
+++ jdk/test/jdk/jfr/startupargs/TestMemoryOptions.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2016, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -493,6 +493,7 @@ public class TestMemoryOptions {
             ProcessBuilder pb;
             if (flightRecorderOptions != null) {
                 pb = ProcessTools.createJavaProcessBuilder(true,
+                                                           "-Xmx256m",
                                                            flightRecorderOptions,
                                                            "-XX:StartFlightRecording",
                                                            SUT.class.getName(),
@@ -500,6 +501,7 @@ public class TestMemoryOptions {
             } else {
                 // default, no FlightRecorderOptions passed
                 pb = ProcessTools.createJavaProcessBuilder(true,
+                                                           "-Xmx256m",
                                                            "-XX:StartFlightRecording",
                                                            SUT.class.getName(),
                                                            tc.getTestName());
