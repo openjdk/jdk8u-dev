ibmruntimes/openj9-openjdk-jdk17

8274893: Update java.desktop classes to use try-with-resources
Backport-of: 70c6df6be431fe11c5441986ed04040f9ec3b750

src/java.desktop/share/classes/com/sun/java/swing/plaf/gtk/Metacity.java
@@ -583,15 +583,16 @@ class Metacity implements SynthConstants {
                     URL url = new URL(new File(userHome).toURI().toURL(),
                                       ".gconf/apps/metacity/general/%25gconf.xml");
                     // Pending: verify character encoding spec for gconf
-                    Reader reader = new InputStreamReader(url.openStream(),
-                                                          ISO_8859_1);
-                    char[] buf = new char[1024];
                     StringBuilder sb = new StringBuilder();
-                    int n;
-                    while ((n = reader.read(buf)) >= 0) {
-                        sb.append(buf, 0, n);
+                    try (InputStream in = url.openStream();
+                         Reader reader = new InputStreamReader(in, ISO_8859_1))
+                    {
+                        char[] buf = new char[1024];
+                        int n;
+                        while ((n = reader.read(buf)) >= 0) {
+                            sb.append(buf, 0, n);
+                        }
                     }
-                    reader.close();
                     String str = sb.toString();
                     if (str != null) {
                         String strLowerCase = str.toLowerCase();

openjdk/jdk8u-dev

jdk/src/share/classes/com/sun/java/swing/plaf/gtk/Metacity.java
@@ -531,14 +531,16 @@ class Metacity implements SynthConstants {
                     URL url = new URL(new File(userHome).toURI().toURL(),
                                       ".gconf/apps/metacity/general/%25gconf.xml");
                     // Pending: verify character encoding spec for gconf
-                    Reader reader = new InputStreamReader(url.openStream(), "ISO-8859-1");
-                    char[] buf = new char[1024];
                     StringBuffer strBuf = new StringBuffer();
-                    int n;
-                    while ((n = reader.read(buf)) >= 0) {
-                        strBuf.append(buf, 0, n);
+                    try (InputStream in = url.openStream();
+                         Reader reader = new InputStreamReader(in, "ISO-8859-1"))
+                    {
+                        char[] buf = new char[1024];
+                        int n;
+                        while ((n = reader.read(buf)) >= 0) {
+                            strBuf.append(buf, 0, n);
+                        }
                     }
-                    reader.close();
                     String str = strBuf.toString();
                     if (str != null) {
                         String strLowerCase = str.toLowerCase();